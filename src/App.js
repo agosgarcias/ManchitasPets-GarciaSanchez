3372987bead0077f46d10fc300d87c8dc67cc4f4
AgosGarcia <agosgarcias@gmail.com>
2021-06-14 20:49:38 -0300

    crear NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+NavBar();
+
+export default App;
